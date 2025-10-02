069beba9472e1b8e00ae41b040bb2da328dbccdf
Thomas Galvaing <thomas.galvaing@akeneo.com>
2025-10-02 17:04:01 +0200

    Fix start when AppToken

--- examples/common/token.mjs
+++ examples/common/token.mjs
@@ -29,6 +29,13 @@ import { updateEnvVar } from './utils.mjs';
   const tokenCreatedAt = process.env.TOKEN_CREATED_AT;
   const appToken = process.env.APP_TOKEN;
 
+  // If APP_TOKEN is defined, always use it and ignore refresh logic
+  if (appToken) {
+    console.error('APP_TOKEN in the .env file. Using it.');
+    updateEnvVar('API_TOKEN', appToken);
+    return;
+  }
+
   if (!clientId || !clientSecret || !username || !password || !pimHost) {
     console.error('Error: Please define CLIENT_ID, CLIENT_SECRET, USERNAME, PASSWORD and PIM_HOST in the .env file');
     process.exit(1);
@@ -88,12 +95,6 @@ import { updateEnvVar } from './utils.mjs';
   };
 
   // Main execution
-  // If APP_TOKEN is defined, always use it and ignore refresh logic
-  if (appToken) {
-    console.error('APP_TOKEN in the .env file. Using it.');
-    updateEnvVar('API_TOKEN', appToken);
-    return;
-  }
   if (apiToken && isTokenStillValid()) {
     console.log('API_TOKEN still valid. Using it.');
     return;
