e6a5fc1d215ba5a4e978fb4bb9da779893fef9b1
JVerbrugge <julien.verbrugge@akeneo.com>
2025-11-20 16:12:15 +0100

    remove div

--- examples/generic_dashboard/src/components/DesignSystemSelector.tsx
+++ examples/generic_dashboard/src/components/DesignSystemSelector.tsx
@@ -7,28 +7,26 @@ export const DesignSystemSelector = () => {
 
   return (
     <ThemeProvider theme={pimTheme}>
-      <div style={{ padding: '20px 20px 0 20px', backgroundColor: '#f7f7fb' }}>
-        <TabBar moreButtonTitle="More">
-          <TabBar.Tab
-            isActive={designSystem === 'akeneo'}
-            onClick={() => setDesignSystem('akeneo')}
-          >
-            Akeneo Design System
-          </TabBar.Tab>
-          <TabBar.Tab
-            isActive={designSystem === 'shadcn'}
-            onClick={() => setDesignSystem('shadcn')}
-          >
-            shadcn/ui
-          </TabBar.Tab>
-          <TabBar.Tab
-            isActive={designSystem === 'polaris'}
-            onClick={() => setDesignSystem('polaris')}
-          >
-            Shopify Polaris
-          </TabBar.Tab>
-        </TabBar>
-      </div>
+      <TabBar moreButtonTitle="More">
+        <TabBar.Tab
+          isActive={designSystem === 'akeneo'}
+          onClick={() => setDesignSystem('akeneo')}
+        >
+          Akeneo Design System
+        </TabBar.Tab>
+        <TabBar.Tab
+          isActive={designSystem === 'shadcn'}
+          onClick={() => setDesignSystem('shadcn')}
+        >
+          Shadcn/UI
+        </TabBar.Tab>
+        <TabBar.Tab
+          isActive={designSystem === 'polaris'}
+          onClick={() => setDesignSystem('polaris')}
+        >
+          Shopify Polaris
+        </TabBar.Tab>
+      </TabBar>
     </ThemeProvider>
   );
 };
